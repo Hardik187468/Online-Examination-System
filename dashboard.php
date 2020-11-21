8939231f41de3f3e1ee352b1218a0e2ecd7a4a63
hardik7468 <hardik187468@gmail.com>
2020-11-21 13:07:59 +0530

    Added code

---
+++ dashboard.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+		<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+		<link rel="stylesheet" href="center/css/centerfont.css">
+		<link href="center/css/jquery/jquery-ui-1.10.3.custom.css" media="screen" rel="stylesheet" type="text/css" />
+		<link href="center/css/center.css" media="screen" rel="stylesheet" type="text/css" />		
+		<title>OES</title>
+		<meta name="description" content="Online Examination system for Educational Organizations">
+		<meta name="keywords" content="Online Examination System">
+		<link rel="stylesheet" href="assets/style.css"> 
+	</head>
+	<body style="background-color: #EAEDF1;">
+		<div id="main_body">
+			<div class="container">
+				<div>
+					<div style="margin-top:10px">
+						<center>
+							
+						<div style="padding-top:20px;">
+						<img src="assets/images/logo/your_school.png"  > 
+</div>				 	
+							<div style="clear:both;color:#aaa; ">
+						<div style="padding-top:14px;">
+	<center>&copy; 2020  <a href="dashboard.php" target="_blank">Online Examination System</a></center>
+</div>
+						<div style="margin:auto; width:100%;">
+							
+							<ul>
+							<li>
+								<section class="center">
+								<a href="login.html">
+								<div class="text">
+								<h2>ADMIN</h2>
+								</div>
+								</a>
+								</section>
+							</li>
+							<li>
+								<section class="student">
+								<a href="login.html">
+								<div class="text">
+								<h2>STUDENT</h2>
+								</div>
+								</a>
+								</section>
+							</li>
+							
+							<li>
+								<section class="admin">
+								<a href="login.html">
+								<div class="text">
+								<h2>TEACHER</h2>
+								</div>
+								</a>
+								</section> 
+							</li>
+							</ul>
+						</div>
+						</center>
+						<div style="clear:both;color:#aaa; ">
+						<div style="float:left;padding-left:160px; " >
+<p>Created by:HardikJain and GiteshPrajapati</p>
+</div>
+			
+	<center></center>
+</div>  					</div>
+				</div>
+			</div>
+		</div>    
+	</body>
+
+</html>
---
+++ teacher/includes/footer.php
@@ -0,0 +1,4 @@
+ <div style="   position: fixed;left: 0;bottom: 0;width: 100%;background-color: #37474f;color: white;text-align: center;padding-top:15px;">
+
+  <p>© 2020 Online Exam | Delhi Technological University | Department of Software Engineering </p>
+</div>
